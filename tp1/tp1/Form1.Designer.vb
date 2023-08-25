e5786b04d55a3acf0393bddff8430287446230f6
joaquin-ac <142785697+joaquin-ac@users.noreply.github.com>
2023-08-25 20:14:04 -0300

    Add files via upload

---
+++ tp1/tp1/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.BGuardar = New System.Windows.Forms.Button()
+        Me.BEliminar = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LApellido = New System.Windows.Forms.TextBox()
+        Me.LNombre = New System.Windows.Forms.TextBox()
+        Me.TResult = New System.Windows.Forms.TextBox()
+        Me.BSalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "apellido"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(260, 269)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(36, 269)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'BGuardar
+        '
+        Me.BGuardar.Location = New System.Drawing.Point(46, 213)
+        Me.BGuardar.Name = "BGuardar"
+        Me.BGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.BGuardar.TabIndex = 3
+        Me.BGuardar.Text = "guardar"
+        Me.BGuardar.UseVisualStyleBackColor = True
+        '
+        'BEliminar
+        '
+        Me.BEliminar.Location = New System.Drawing.Point(148, 213)
+        Me.BEliminar.Name = "BEliminar"
+        Me.BEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.BEliminar.TabIndex = 4
+        Me.BEliminar.Text = "eliminar"
+        Me.BEliminar.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "nombre"
+        '
+        'LApellido
+        '
+        Me.LApellido.Location = New System.Drawing.Point(74, 54)
+        Me.LApellido.Name = "LApellido"
+        Me.LApellido.Size = New System.Drawing.Size(100, 20)
+        Me.LApellido.TabIndex = 6
+        '
+        'LNombre
+        '
+        Me.LNombre.Location = New System.Drawing.Point(74, 100)
+        Me.LNombre.Name = "LNombre"
+        Me.LNombre.Size = New System.Drawing.Size(100, 20)
+        Me.LNombre.TabIndex = 7
+        '
+        'TResult
+        '
+        Me.TResult.Location = New System.Drawing.Point(304, 57)
+        Me.TResult.Multiline = True
+        Me.TResult.Name = "TResult"
+        Me.TResult.Size = New System.Drawing.Size(140, 124)
+        Me.TResult.TabIndex = 8
+        '
+        'BSalir
+        '
+        Me.BSalir.AllowDrop = True
+        Me.BSalir.Location = New System.Drawing.Point(353, 213)
+        Me.BSalir.Name = "BSalir"
+        Me.BSalir.Size = New System.Drawing.Size(75, 23)
+        Me.BSalir.TabIndex = 9
+        Me.BSalir.Text = "salir" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.BSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 261)
+        Me.Controls.Add(Me.BSalir)
+        Me.Controls.Add(Me.TResult)
+        Me.Controls.Add(Me.LNombre)
+        Me.Controls.Add(Me.LApellido)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BEliminar)
+        Me.Controls.Add(Me.BGuardar)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "mi prime form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents BGuardar As Button
+    Friend WithEvents BEliminar As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LApellido As TextBox
+    Friend WithEvents LNombre As TextBox
+    Friend WithEvents TResult As TextBox
+    Friend WithEvents BSalir As Button
+End Class
---
+++ tp1/tp1/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub Form1_keyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+
+
+        If (e.KeyCode = Keys.S AndAlso e.Modifiers = Keys.Control) Then
+            e.Handled = True
+            Button2_Click(sender, e) 'or cmdExit.PerformClick()'
+        End If
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles LNombre.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles LApellido.TextChanged
+
+    End Sub
+
+    Private Sub BGuardar_Click(sender As Object, e As EventArgs) Handles BGuardar.Click
+        TResult.Text = LApellido.Text + " " + LNombre.Text
+    End Sub
+
+    Private Sub TResult_TextChanged(sender As Object, e As EventArgs) Handles TResult.TextChanged
+
+    End Sub
+
+    Private Sub BEliminar_Click(sender As Object, e As EventArgs) Handles BEliminar.Click
+        TResult.Text = ""
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles BSalir.Click
+
+
+        End
+    End Sub
+End Class
