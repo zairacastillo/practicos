e9272936d143bb83168cd23ac16f40c1bb07971e
joaquin-ac <joaquin.acb36@gmail.com>
2023-08-29 03:20:19 -0300

    2

---
+++ tp2/tp2/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        LNyA = New Label()
+        LDni = New Label()
+        LApellido = New Label()
+        LNombre = New Label()
+        LModificar = New Label()
+        TGuardar = New Button()
+        TEliminar = New Button()
+        TDni = New TextBox()
+        TNombre = New TextBox()
+        TApellido = New TextBox()
+        SuspendLayout()
+        ' 
+        ' LNyA
+        ' 
+        LNyA.AutoSize = True
+        LNyA.Location = New Point(32, 51)
+        LNyA.Name = "LNyA"
+        LNyA.Size = New Size(110, 15)
+        LNyA.TabIndex = 0
+        LNyA.Text = "Nombre y Apellido:"
+        ' 
+        ' LDni
+        ' 
+        LDni.AutoSize = True
+        LDni.Location = New Point(32, 97)
+        LDni.Name = "LDni"
+        LDni.Size = New Size(27, 15)
+        LDni.TabIndex = 1
+        LDni.Text = "DNI"
+        ' 
+        ' LApellido
+        ' 
+        LApellido.AutoSize = True
+        LApellido.Location = New Point(32, 139)
+        LApellido.Name = "LApellido"
+        LApellido.Size = New Size(51, 15)
+        LApellido.TabIndex = 2
+        LApellido.Text = "Apellido"
+        ' 
+        ' LNombre
+        ' 
+        LNombre.AutoSize = True
+        LNombre.Location = New Point(32, 185)
+        LNombre.Name = "LNombre"
+        LNombre.Size = New Size(51, 15)
+        LNombre.TabIndex = 3
+        LNombre.Text = "Nombre"
+        ' 
+        ' LModificar
+        ' 
+        LModificar.AutoSize = True
+        LModificar.ForeColor = Color.Red
+        LModificar.Location = New Point(259, 51)
+        LModificar.Name = "LModificar"
+        LModificar.Size = New Size(58, 15)
+        LModificar.TabIndex = 4
+        LModificar.Text = "modificar"
+        ' 
+        ' TGuardar
+        ' 
+        TGuardar.ForeColor = Color.Black
+        TGuardar.Location = New Point(98, 265)
+        TGuardar.Name = "TGuardar"
+        TGuardar.Size = New Size(75, 23)
+        TGuardar.TabIndex = 5
+        TGuardar.Text = "Guardar"
+        TGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' TEliminar
+        ' 
+        TEliminar.ForeColor = Color.Black
+        TEliminar.Location = New Point(259, 265)
+        TEliminar.Name = "TEliminar"
+        TEliminar.Size = New Size(75, 23)
+        TEliminar.TabIndex = 6
+        TEliminar.Text = "Eliminar"
+        TEliminar.UseVisualStyleBackColor = True
+        ' 
+        ' TDni
+        ' 
+        TDni.Location = New Point(227, 101)
+        TDni.Name = "TDni"
+        TDni.Size = New Size(136, 23)
+        TDni.TabIndex = 7
+        ' 
+        ' TNombre
+        ' 
+        TNombre.Location = New Point(227, 185)
+        TNombre.Name = "TNombre"
+        TNombre.Size = New Size(136, 23)
+        TNombre.TabIndex = 8
+        ' 
+        ' TApellido
+        ' 
+        TApellido.Location = New Point(227, 139)
+        TApellido.Name = "TApellido"
+        TApellido.Size = New Size(136, 23)
+        TApellido.TabIndex = 9
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(490, 450)
+        Controls.Add(TApellido)
+        Controls.Add(TNombre)
+        Controls.Add(TDni)
+        Controls.Add(TEliminar)
+        Controls.Add(TGuardar)
+        Controls.Add(LModificar)
+        Controls.Add(LNombre)
+        Controls.Add(LApellido)
+        Controls.Add(LDni)
+        Controls.Add(LNyA)
+        Name = "Form1"
+        Text = "Pequeño Formulario"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents LNyA As Label
+    Friend WithEvents LDni As Label
+    Friend WithEvents LApellido As Label
+    Friend WithEvents LNombre As Label
+    Friend WithEvents LModificar As Label
+    Friend WithEvents TGuardar As Button
+    Friend WithEvents TEliminar As Button
+    Friend WithEvents TDni As TextBox
+    Friend WithEvents TNombre As TextBox
+    Friend WithEvents TApellido As TextBox
+End Class
---
+++ tp2/tp2/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+
+
+    'Funcion para que solo permite el ingreso de caracteres tipo letra
+    Sub SoloLetras(ByRef e As System.Windows.Forms.KeyPressEventArgs)
+        If Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+            MsgBox("Solo se puede ingresar valores de tipo texto", MsgBoxStyle.Exclamation, "Error")
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = False
+        End If
+    End Sub
+
+
+    'Funcion para que solo permite el ingreso de caracteres tipo numerico
+    Sub SoloNumeros(ByRef e As System.Windows.Forms.KeyPressEventArgs)
+        If Char.IsDigit(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+            MsgBox("Solo se puede ingresar valores de tipo número", MsgBoxStyle.Exclamation, "Error")
+        End If
+    End Sub
+
+    Private Sub Tapellido_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TApellido.KeyPress
+        SoloLetras(e)
+    End Sub
+
+    Private Sub Tnombre_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TNombre.KeyPress
+        SoloLetras(e)
+    End Sub
+
+    Private Sub Tdni_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TDni.KeyPress
+        SoloNumeros(e)
+    End Sub
+
+    Private Sub TGuardar_Click(sender As Object, e As EventArgs) Handles TGuardar.Click
+        LModificar.Text = TNombre.Text + " " + TApellido.Text
+        Dim ask As MsgBoxResult
+
+        If (TDni.Text Is "" OrElse TNombre.Text Is "" OrElse TApellido.Text Is "") Then
+            MsgBox("Debe completar todos los campos", MsgBoxStyle.Critical, "Error")
+        Else
+            ask = MsgBox("¿Seguro desea insertar un nuevo cliente?", vbYesNo + vbQuestion, "confirmar insersion")
+
+            If ask = vbYes Then
+                MsgBox("El cliente: " + LModificar.Text + " se inserto correctamente", MsgBoxStyle.Information, "Guardar")
+            End If
+        End If
+    End Sub
+
+
+    Private Sub TEliminar_Click(sender As Object, e As EventArgs) Handles TEliminar.Click
+
+        Dim ask As MsgBoxResult
+        ask = MsgBox("Está apunto de eliminar el cliente: " + TNombre.Text + " " + TApellido.Text, vbYesNo + vbExclamation + vbDefaultButton2, "confirmar eliminacion")
+
+        If ask = vbYes Then
+            MsgBox("El cliente: " + TNombre.Text + " " + TApellido.Text + " se elimino correctamente", MsgBoxStyle.Information, "Eliminar")
+            LModificar.Text = "modificar"
+            TNombre.Clear()
+            TApellido.Clear()
+            TDni.Clear()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
+
+
